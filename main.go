0c438dce69111ab3d999fcc65cbcafa2853fffb3
md96 <mahesh.tella@gmail.com>
2025-06-17 14:25:50 +0530

    Array and slices added  commit

--- main.go
+++ main.go
@@ -6,4 +6,36 @@ func main() {
 
 	fmt.Println("Hello world this is go lang program")
 
+	//Array and slice usage
+	var arraydeclearation = [8]int{1, 3, 5, 6, 7, 8, 9}
+
+	fmt.Println("Data inside array :", arraydeclearation)
+
+	slicedata := arraydeclearation[2:3]
+	fmt.Println("Data inside slice: ", slicedata)
+
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+	slicedata = append(slicedata, 4)
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+	slicedata = append(slicedata, 4)
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+
+	slicedata = append(slicedata, 4)
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+
+	slicedata = append(slicedata, 4)
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+	slicedata = append(slicedata, 4)
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+
+	slicedata = append(slicedata, 4)
+	fmt.Println(cap(slicedata))
+	fmt.Println(len(slicedata))
+
 }
